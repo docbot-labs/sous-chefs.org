99af2cb635e2a15f9db99d388f78065a03e7d48b
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2023-08-07 09:36:35 +0100

    Add metadata

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import { Main } from "./components/main";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
   logo: <span>Sous Chefs</span>,
   project: {
     link: "https://github.com/sous-chefs",
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://sous-chefs.org/" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || "Sous Chefs"} />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description || "Community of Chef cookbook maintainers"
+          }
+        />
+      </>
+    );
   },
   docsRepositoryBase: "https://github.com/sous-chefs",
   footer: {
