f86b7bc76d04f2b110178a66763ebc65a9a602c3
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2023-08-17 18:15:01 +0100

    Add powered by footer

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,6 +4,7 @@ import { Main } from "./components/main";
 import { useRouter } from "next/router";
 import logo from "./images/logo.png";
 import Image from "next/image";
+import { Logo } from "./components/doclabs-logo";
 
 const config: DocsThemeConfig = {
   logo: <Image width={48} height={48} src={logo} alt="Sous Chefs" />,
@@ -37,7 +38,15 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: "https://github.com/sous-chefs",
   footer: {
-    text: "Sous Chefs",
+    text: (
+      <a
+        className="flex flex-row items-center gap-2"
+        href="https://www.doclabs.dev"
+        target="__blank"
+      >
+        Powered by <Logo className="h-3 inline-block" />
+      </a>
+    ),
   },
   main: Main,
 };
